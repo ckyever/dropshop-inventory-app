4a42cdaf6025d9537ba58deb1b684c65a67b5385
Ckyever Gaviola <ckyeverg@gmail.com>
2025-11-27 10:10:17 +1100

    Add category links to the home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,9 @@
 import { sendToPage } from "./utils.js";
+import { getCategories } from "../db/queries.js";
 
-const getHomePage = (req, res) => {
-  sendToPage(res, "pages/home");
+const getHomePage = async (req, res) => {
+  const categories = await getCategories();
+  sendToPage(res, "pages/home", { categories: categories });
 };
 
 export { getHomePage };
